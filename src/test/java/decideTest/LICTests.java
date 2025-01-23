dcafd68885f87a6f7175a48740df2b3cf9558a8a
FMurkz <feliciamurkes@gmail.com>
2025-01-23 23:24:22 +0100

    refactor: Changed test_condition0_Valid() to simpler input (#17)

--- src/test/java/decideTest/LICTests.java
+++ src/test/java/decideTest/LICTests.java
@@ -12,8 +12,8 @@ public class LICTests {
     @Test
     public void test_condition0_Valid() {
         LIC lic = new LIC();
-        double[] xList = new double[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
-        double[] yList = new double[]{2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        double[] xList = new double[]{1, 3};
+        double[] yList = new double[]{1, 4};
         int length1 = 2;
         boolean result = lic.condition0(xList, yList,length1);
         assertTrue(result, "Expected true because the distance between two points is greater than length1");
