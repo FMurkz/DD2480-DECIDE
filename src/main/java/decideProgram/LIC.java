8e1e1bf69dbd4e7d7224e77bf52dfba7a989781a
FMurkz <feliciamurkes@gmail.com>
2025-01-23 17:21:56 +0100

    feat: Implemented condition 0 (#17)

--- src/main/java/decideProgram/LIC.java
+++ src/main/java/decideProgram/LIC.java
@@ -2,8 +2,24 @@ package decideProgram;
 
 public class LIC {
 
-    public boolean condition0() {
-        return true;
+    /**
+     * LIC 0: There exists at least one set of two 
+     * consecutive data points that are a distance greater 
+     * than the length, LENGTH1, apart.
+     */
+    public boolean condition0(double[] xArray, double[] yArray, int length1) {
+
+        if (xArray.length!=yArray.length) throw new IllegalArgumentException("xArray and yArray must have the same length");
+
+        if (length1 < 0) throw new IllegalArgumentException("Length1 must be greater than or equal to 0");
+
+        if (xArray.length < 2) return false;
+
+        for (int i = 0; i < xArray.length - 1; i++) {
+            double distance = Math.sqrt(Math.pow(xArray[i + 1] - xArray[i], 2) + Math.pow(yArray[i + 1] - yArray[i], 2));
+            if (distance > length1) return true;
+        }
+        return false;
     }
 
     public boolean condition1() {
