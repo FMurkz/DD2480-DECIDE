916210628ae3ae4efe748a694086ce2b3f494550
Felicia Murkes <112473043+FMurkz@users.noreply.github.com>
2025-01-23 13:31:24 +0100

    Issue/15  setup for tests (#16)
    * Issue/15 Added pom.xml file (#15)
    
    * Issue/15 Added testfile for LICs (#15)

---
+++ src/test/java/decideTest/LICTests.java
@@ -0,0 +1,13 @@
+package test.java.decideTest;
+import decideProgram.LIC;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LICTests {
+
+    @Test
+    public void testCondition0() {
+        LIC lic = new LIC();
+        assertTrue(lic.condition0(), "condition0 should return true");
+    }
+}
