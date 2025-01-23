d89ce01bd9b99f908fd56f17a001dc85799dbc28
murkes <murkes@kth.se>
2025-01-23 01:34:38 +0100

    Issue/11 - Added correct global variables (#11)

--- src/assignment1/Decide.java
+++ src/assignment1/Decide.java
@@ -1,8 +1,14 @@
 package assignment1;
 
 public class Decide {
+    public int numPoints;
+    public double[] x;
+    public double[] y;
+    public int[][] lcm;
+    public boolean[] puv;
+    public Parameters param;
 
-    public static boolean DECIDE(Input input){
+    public static boolean DECIDE(){
         return true;
     }
 }
