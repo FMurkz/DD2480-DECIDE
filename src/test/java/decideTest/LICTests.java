addc1cc1f6b5fa18893897a6aeb178a25a7bf0d0
FMurkz <feliciamurkes@gmail.com>
2025-01-23 17:14:37 +0100

    test: Added test for condition 0 with valid input (#17)

--- src/test/java/decideTest/LICTests.java
+++ src/test/java/decideTest/LICTests.java
@@ -5,9 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LICTests {
 
+    /**
+     * LIC0
+     * Test case where the distance between two points is greater than length1
+     */
     @Test
-    public void testCondition0() {
+    public void test_condition0_Valid() {
         LIC lic = new LIC();
-        assertTrue(lic.condition0(), "condition0 should return true");
+        double[] xList = new double[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+        double[] yList = new double[]{2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        int length1 = 2;
+        boolean result = lic.condition0(xList, yList,length1);
+        assertTrue(result, "Expected true because the distance between two points is greater than length1");
     }
 }
