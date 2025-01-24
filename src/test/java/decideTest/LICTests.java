a8081ce709ae17a1793fbdb21c1ed649faab451a
Melissa Saber Tehrani <melst@kth.se>
2025-01-24 15:14:35 +0100

    test: added a test for condition 2 with less then 3 datapoints (#23)

--- src/test/java/decideTest/LICTests.java
+++ src/test/java/decideTest/LICTests.java
@@ -236,5 +236,21 @@ public class LICTests {
         int length1 = 1;
         assertThrows(IllegalArgumentException.class, () -> lic.condition7(xList, yList, numPoints, kPts, length1), "Expected IllegalArgumentException because KPTS is less than 1");
     }
+
+    /**
+     * LIC2
+     * Test case with too few points
+     */
+    @Test
+    public void test_condition2_NotEnoughPoints() {
+        LIC lic = new LIC();
+        double[] xList = {0, 1};
+        double[] yList = {0, 1};
+        int numPoints = 2;
+        double epsilon = 0.5;
+
+        boolean result = lic.condition2(xList, yList, numPoints, epsilon);
+        assertFalse(result, "Expected false because there are fewer than 3 points.");
+    }
 }
 
