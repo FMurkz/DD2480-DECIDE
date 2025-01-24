8e498fb12acbbe4a9d804071dcf3451731fef174
FMurkz <feliciamurkes@gmail.com>
2025-01-24 11:39:20 +0100

    feat: Implemented condition 7 #19

--- src/main/java/decideProgram/LIC.java
+++ src/main/java/decideProgram/LIC.java
@@ -46,9 +46,28 @@ public class LIC {
         return true;
     }
 
-    public boolean condition7() {
-        return true;
+    /**
+     * LIC 7: 
+     * There exists at least one set of two data points separated by exactly
+     * K PTS consecutive intervening points that are a distance greater than the length, LENGTH1, apart. 
+     * The condition is not met when NUMPOINTS < 3.
+     */
+    public boolean condition7(double[] xArray, double[] yArray, int numPoints, int kPts, int length1) {
+        if (xArray.length!=yArray.length) throw new IllegalArgumentException("xArray and yArray must have the same length");
+        if (numPoints < 3) return false;
+        if (kPts <= 0) throw new IllegalArgumentException("kPts must be greater than or equal to 1");
+
+        for (int i = 0; i < numPoints - kPts - 1; i++) {
+            double distance = Math.sqrt(
+                Math.pow(xArray[i + kPts + 1] - xArray[i], 2) + 
+                Math.pow(yArray[i + kPts + 1] - yArray[i], 2)
+            );
+            if (distance > length1) return true; // Condition satisfied
+        }
+    
+        return false; // Condition not satisfied
     }
+
 
     public boolean condition8() {
         return true;
