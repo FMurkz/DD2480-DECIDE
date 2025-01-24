dca2671285504ae05f1bf5c047c1a112c3eceb75
Felicia Murkes <112473043+FMurkz@users.noreply.github.com>
2025-01-24 10:38:26 +0100

    Issue/17 Implement condition 0 (#27)
    * test: Added test for condition 0 with valid input (#17)
    
    * test: Added test for condition 0 with invalid input (#17)
    
    * test: Added test for condition 0, no datapoints (#17)
    
    * test: Added test for condition 0, one datapoint (#17)
    
    * feat: Implemented condition 0 (#17)
    
    * refactor: Changed test_condition0_Valid() to simpler input (#17)

--- src/test/java/decideTest/LICTests.java
+++ src/test/java/decideTest/LICTests.java
@@ -5,9 +5,60 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LICTests {
 
+    /**
+     * LIC0
+     * Test case where the distance between two points is greater than length1
+     */
     @Test
-    public void testCondition0() {
+    public void test_condition0_Valid() {
         LIC lic = new LIC();
-        assertTrue(lic.condition0(), "condition0 should return true");
+        double[] xList = new double[]{1, 3};
+        double[] yList = new double[]{1, 4};
+        int length1 = 2;
+        boolean result = lic.condition0(xList, yList,length1);
+        assertTrue(result, "Expected true because the distance between two points is greater than length1");
     }
+
+    /**
+     * LIC0
+     * Test case where the distance between two points is less than length1
+     */
+    @Test
+    public void test_condition0_Invalid() {
+        LIC lic = new LIC();
+        double[] xList = new double[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        double[] yList = new double[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int length1 = 10;
+        boolean result = lic.condition0(xList, yList, length1);
+        assertFalse(result, "Expected false because the distance is always less than length1");
+    }
+
+    /**
+     * LIC0
+     * Test case where the there are no data points
+     */
+    @Test
+    public void test_condition0_No_datapoints() {
+        LIC lic = new LIC();
+        double[] xList = new double[]{};
+        double[] yList = new double[]{};
+        int length1 = 2;
+        boolean result = lic.condition0(xList, yList, length1);
+        assertFalse(result, "Expected false because there are no data points");
+    }
+
+    /**
+     * LIC0
+     * Test case where there is only one data point
+     */
+    @Test
+    public void test_condition0_One_datapoint() {
+        LIC lic = new LIC();
+        double[] xList = new double[]{0};
+        double[] yList = new double[]{0};
+        int length1 = 2;
+        boolean result = lic.condition0(xList, yList, length1);
+        assertFalse(result, "Expected false because there is only one data point");
+    }
+
 }
